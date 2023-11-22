071fd572da32f5e5a037578ad26572a8debb52be
Marucha <marucha.webdev@yahoo.com>
2023-11-22 09:47:33 +0300

    style: JOb Component styling

--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,7 +1,7 @@
 
 export const Container = ({ children }) => {
   return (
-    <div className="tablet:container m-auto p-4 space-y-6">
+    <div className="tablet:container m-auto p-4 space-y-8">
         {children}
     </div>
   )
--- src/Components/JobList.js
+++ src/Components/JobList.js
@@ -9,26 +9,26 @@ const Job = ({
   const featuredBrd = featured ? "border-l-4 border-l-Dark-Grayish-Cyan" : ""
 
   return (
-    <section className={`p-4 gap-3 bg-[#ffff] rounded-l-sm rounded-lg shadow-2xl ${featuredBrd}`}>
-       {/* <img src={logo} alt={""} className="bg-[#fafafa]" /> */}
+    <section className={`p-4 flex flex-col gap-4 items-start tablet:flex-row bg-[#ffff] rounded-l-sm rounded-lg shadow-2xl ${featuredBrd}`}>
+       <img src={logo} alt={""} className="bg-[#000000] p-10 mt-[-45px] rounded-full tablet:mt-0" />
        <div>
-          <article className='flex gap-4 items-center'>
-            <p className=' text-Dark-Grayish-Cyan font-semibold'>{company}</p>
-            <p className='rounded-2xl font-semibold'>{recent ? "New" : "" }</p>
-            <p className='rounded-2xl font-semibold'>{featured ? "Featured" : "" }</p>
+          <article className='flex gap-3 space-x-2 items-center'>
+            <p className='text-Dark-Grayish-Cyan font-semibold'>{company}</p>
+            <p hidden={!recent}   className='rounded-md font-semibold p-1 text-Light-Grayish-Cyan bg-Dark-Grayish-Cyan'>{recent ? "New" : "" }</p>
+            <p hidden={!featured} className='rounded-md font-semibold p-1 text-Light-Grayish-Cyan bg-Very-Dark-Grayish-Cyan'>{featured ? "Featured" : "" }</p>
           </article>
 
-          <article className='flex flex-col tablet:flex-col'>
-            <div className='flex flex-col items-start tablet:flex-row tablet:items-center gap-3 justify-between'>
+          <article>
+            <div className='flex flex-col items-start laptop:flex-row laptop:items-center gap-3 justify-between'>
               <p className='text-Very-Dark-Grayish-Cyan hover:text-Dark-Grayish-Cyan font-semibold'>{position}</p>
-              <p className='flex gap-1 items-center overflow-clip text-ellipsis'>
+              <p className='flex grow gap-1 items-center justify-between text-ellipsis'>
                 <button className='p-1.5 bg-Light-Grayish-Cyan rounded hover:bg-Dark-Grayish-Cyan hover:text-Light-Grayish-Cyan' onClick={() => {filterFunc("level", level)}} >{level}</button>
                 <button className='p-1.5 bg-Light-Grayish-Cyan rounded hover:bg-Dark-Grayish-Cyan hover:text-Light-Grayish-Cyan' onClick={() => {filterFunc("role",role)}}>{role}</button>
-                { languages.map((lang) =>
-                  (<button className='p-1.5 bg-Light-Grayish-Cyan rounded hover:bg-Dark-Grayish-Cyan hover:text-Light-Grayish-Cyan' onClick={() => {filterArray("languages", lang)}} key={uuid()}>
-                      {lang}
-                  </button>
-                ))}
+                  { languages.map((lang) =>
+                    (<button className='p-1.5 bg-Light-Grayish-Cyan rounded hover:bg-Dark-Grayish-Cyan hover:text-Light-Grayish-Cyan' onClick={() => {filterArray("languages", lang)}} key={uuid()}>
+                        {lang}
+                    </button>
+                  ))}
                 { tools.map((t) =>
                   (<button className='p-1.5 bg-Light-Grayish-Cyan rounded hover:bg-Dark-Grayish-Cyan hover:text-Light-Grayish-Cyan' onClick={() => filterArray("tools", t)} key={uuid()}>
                     {t}
@@ -50,7 +50,7 @@ const Job = ({
 
 export const JobList = ({ jobs, filterCriteria, filterKeyArray }) => {
   return (
-    <div className="flex flex-col gap-4 m-auto">
+    <div className="flex flex-col gap-10 m-auto">
       { jobs.map((job) => <Job key={job.id} {...job} filterFunc={filterCriteria} filterArray={filterKeyArray}/> )}
     </div>
   )
